c90e52fe450916605179945563c84404e1eb9b82
venkat20051 <venkatkotha456@gmail.com>
2026-01-08 18:38:42 +0530

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,8 @@ app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/tasks", require("./routes/taskRoutes"));
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+

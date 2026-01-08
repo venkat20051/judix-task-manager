aa9ef0826dd9abd47c364ecfe1cf2cda738cc43e
venkat20051 <venkatkotha456@gmail.com>
2026-01-08 19:26:31 +0530

    Update frontend to use deployed backend API

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://judix-task-manager.onrender.com/api"
 });
-
 // Attach token to every request
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
